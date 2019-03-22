7d2e2bef7fdc42a9f34d9282f529d32895dc018c
zbychhhu <grzegorzmarchewka351@gmail.com>
2019-03-22 14:13:43 +0100

    add Employee interface

---
+++ src/main/java/com/servicecompany/agh/Employee.java
@@ -0,0 +1,13 @@
+package com.servicecompany.agh;
+
+public interface Employee {
+
+    String getName();
+
+    String getSurname();
+
+    Integer getExperience();
+
+    Integer getExperienceInCompany();
+
+}
