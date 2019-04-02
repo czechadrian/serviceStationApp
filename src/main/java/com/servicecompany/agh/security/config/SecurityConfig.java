6b4e7e6d0b1059d4cb9fb33270b05457fd8bf4e8
czanciszek <fyczana48@gmail.com>
2019-04-02 20:58:39 +0200

    Connected to AWS Database Implemented Employee: DAO, Service and Controller Implemented simple function: getEmployeeById //UPDATE: Added GET Request Method on /users mapping to see all users Modified columns to see name of the user's role //UPDATE: Added Authentication connected with database

--- src/main/java/com/servicecompany/agh/security/config/SecurityConfig.java
+++ src/main/java/com/servicecompany/agh/security/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,16 +19,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+
+
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+        final String sqlCount = "SELECT COUNT(*) FROM USER";
+        int count = jdbcTemplate.queryForObject(sqlCount, new Object[] {}, Integer.class);
 
+        final String sqlLogin = "SELECT login FROM USER WHERE USER.id=?";
+        final String sqlPassword = "SELECT password FROM USER WHERE USER.id=?";
+        final String sqlRole = "SELECT role FROM USER JOIN ROLE ON USER.id_role=ROLE.id WHERE USER.id=?";
+
+        for( int i=1; i <= count; i++ ) {
+
+            String login = jdbcTemplate.queryForObject(sqlLogin, new Object[] {i}, String.class);
+            String password = jdbcTemplate.queryForObject(sqlPassword, new Object[] {i}, String.class);
+            String role = jdbcTemplate.queryForObject(sqlRole, new Object[] {i}, String.class).toUpperCase();
+
+            auth.inMemoryAuthentication().passwordEncoder(passwordEncoder)
+                    .withUser(login).password(passwordEncoder.encode(password)).roles(role);
+        }
+        /*
         auth.inMemoryAuthentication()
                 .passwordEncoder(passwordEncoder)
 
                 .withUser("manager").password(passwordEncoder.encode("password")).roles("MANAGER")
                 .and()
                 .withUser("mechanic").password(passwordEncoder.encode("password")).roles("MECHANIC");
+                */
+
     }
 
     @Bean
