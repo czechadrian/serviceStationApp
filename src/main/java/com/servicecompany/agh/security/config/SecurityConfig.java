7e3ccc72f59c553b1665b5dc069dabecce717175
zbychhhu <grzegorzmarchewka351@gmail.com>
2019-04-02 21:48:49 +0200

    add Logger

--- src/main/java/com/servicecompany/agh/security/config/SecurityConfig.java
+++ src/main/java/com/servicecompany/agh/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.servicecompany.agh.security.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,8 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-
-
+    private static final Logger log = LogManager.getLogger(SecurityConfig.class);
 
 
     @Override
@@ -36,7 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         final String sqlPassword = "SELECT password FROM USER WHERE USER.id=?";
         final String sqlRole = "SELECT role FROM USER JOIN ROLE ON USER.id_role=ROLE.id WHERE USER.id=?";
 
+        log.info("searching users in table USER");
         for( int i=1; i <= count; i++ ) {
+
 
             String login = jdbcTemplate.queryForObject(sqlLogin, new Object[] {i}, String.class);
             String password = jdbcTemplate.queryForObject(sqlPassword, new Object[] {i}, String.class);
@@ -45,14 +48,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             auth.inMemoryAuthentication().passwordEncoder(passwordEncoder)
                     .withUser(login).password(passwordEncoder.encode(password)).roles(role);
         }
-        /*
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-
-                .withUser("manager").password(passwordEncoder.encode("password")).roles("MANAGER")
-                .and()
-                .withUser("mechanic").password(passwordEncoder.encode("password")).roles("MECHANIC");
-                */
 
     }
 
