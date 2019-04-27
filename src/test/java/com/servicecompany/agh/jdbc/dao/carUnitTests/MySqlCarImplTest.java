e8309b5ea868fb50217511c1c72a79d1efa9cad9
adi <czech400m@gmail.com>
2019-04-27 17:44:18 +0200

    feature/AGH_33 change paramers in Car object due to failed tests

--- src/test/java/com/servicecompany/agh/jdbc/dao/carUnitTests/MySqlCarImplTest.java
+++ src/test/java/com/servicecompany/agh/jdbc/dao/carUnitTests/MySqlCarImplTest.java
@@ -31,7 +31,7 @@ public class MySqlCarImplTest {
 
     @Test
     public void getCarById() {
-        Car car = new Car(99,"Maciek", 808808808L, "Lanos", "Daewoo", "RJSF520");
+        Car car = new Car(99,"","","Maciek", 808808808L, "Lanos", "Daewoo", "RJSF520");
         carDao.insertCarToDb(car);
         Car testCar = carDao.getCarByRegistrationNumber("RJSF520");
         int id = testCar.getId();
@@ -41,8 +41,8 @@ public class MySqlCarImplTest {
 
     @Test
     public void getCarByOwnerSurname() {
-        Car car1 = new Car(99,"Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
-        Car car2 = new Car(99,"Maciek", 808808809L, "Matiz", "Daewoo", "RJSF523");
+        Car car1 = new Car(99,"","","Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
+        Car car2 = new Car(99,"","","Maciek", 808808809L, "Matiz", "Daewoo", "RJSF523");
         carDao.insertCarToDb(car1);
         carDao.insertCarToDb(car2);
         Collection<Car> listOfCarsBySurnames = carDao.getCarByOwnerSurname("Maciek");
@@ -56,8 +56,8 @@ public class MySqlCarImplTest {
 
     @Test
     public void getCarByOwnerPhoneNumber() {
-        Car car1 = new Car(99,"Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
-        Car car2 = new Car(99,"Maciek", 808808808L, "Matiz", "Daewoo", "RJSF523");
+        Car car1 = new Car(99,"","","Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
+        Car car2 = new Car(99,"","","Maciek", 808808808L, "Matiz", "Daewoo", "RJSF523");
         carDao.insertCarToDb(car1);
         carDao.insertCarToDb(car2);
         Collection<Car> listOfCarsByPhoneNumbers = carDao.getCarByOwnerPhoneNumber("808808808");
@@ -71,7 +71,7 @@ public class MySqlCarImplTest {
 
     @Test
     public void getCarByRegistrationNumber() {
-        Car car = new Car(99,"Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
+        Car car = new Car(99,"","","Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
         carDao.insertCarToDb(car);
         Car testCar = carDao.getCarByRegistrationNumber("RJSF522");
         Assert.assertTrue(testCar.getModel().equals("Lanos"));
@@ -80,8 +80,8 @@ public class MySqlCarImplTest {
 
     @Test
     public void getCarByBrand() {
-        Car car1 = new Car(99,"Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
-        Car car2 = new Car(99,"Maciek", 808808809L, "Matiz", "Daewoo", "RJSF523");
+        Car car1 = new Car(99,"","","Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
+        Car car2 = new Car(99,"","","Maciek", 808808809L, "Matiz", "Daewoo", "RJSF523");
         carDao.insertCarToDb(car1);
         carDao.insertCarToDb(car2);
         Collection<Car> listOfCarsBySurnames = carDao.getCarByBrand("Daewoo");
@@ -95,8 +95,8 @@ public class MySqlCarImplTest {
 
     @Test
     public void getCarByModel() {
-        Car car1 = new Car(99,"Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
-        Car car2 = new Car(99,"Maciek", 808808809L, "Lanos", "Daewoo", "RJSF523");
+        Car car1 = new Car(99,"","","Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
+        Car car2 = new Car(99,"","","Maciek", 808808809L, "Lanos", "Daewoo", "RJSF523");
         carDao.insertCarToDb(car1);
         carDao.insertCarToDb(car2);
         Collection<Car> listOfCarsBySurnames = carDao.getCarByModel("Lanos");
@@ -110,7 +110,7 @@ public class MySqlCarImplTest {
 
     @Test
     public void deleteCarById() {
-        Car car = new Car(99,"Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
+        Car car = new Car(99,"","","Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
         carDao.insertCarToDb(car);
         Collection<Car> listOfCars = carDao.getCarByModel("Lanos");
         Assert.assertFalse(listOfCars.isEmpty());
@@ -123,10 +123,10 @@ public class MySqlCarImplTest {
 
     @Test
     public void updateCarById() {
-        Car car = new Car(99,"Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
+        Car car = new Car(99,"","","Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
         carDao.insertCarToDb(car);
         Car testCar = carDao.getCarByRegistrationNumber("RJSF522");
-        Car newCar = new Car(testCar.getId(),"Maciek", 808808808L, "Nubira", "Daewoo", "RJSF522");
+        Car newCar = new Car(testCar.getId(),"","","Maciek", 808808808L, "Nubira", "Daewoo", "RJSF522");
         carDao.updateCarById(newCar);
         Assert.assertTrue(carDao.getCarById(testCar.getId()).get().getModel().equals("Nubira"));
         carDao.deleteCarById(testCar.getId());
@@ -134,7 +134,7 @@ public class MySqlCarImplTest {
 
     @Test
     public void insertCarToDb() {
-        Car car = new Car(99,"Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
+        Car car = new Car(99,"","","Maciek", 808808808L, "Lanos", "Daewoo", "RJSF522");
         carDao.insertCarToDb(car);
         Collection<Car> listOfCars = carDao.getCarByModel("Lanos");
         Assert.assertFalse(listOfCars.isEmpty());
